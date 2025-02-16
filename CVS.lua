68540a88f44a12a417e71ee6e1bb5ca0d271243c
J0BB0T <155307865+J0BB0T@users.noreply.github.com>
2025-02-16 20:59:51 +0000

    Update CVS.lua

--- CVS.lua
+++ CVS.lua
@@ -1,19 +1,23 @@
-local place_Holder = nil
+getgenv().LoopKill = {}
 
-for i,v in pairs(game.Players.LocalPlayer.Backpack:GetDescendants()) do
-    if v.Name == "InflictTarget" then
-        place_Holder = v
-    end
-end;
+task.spawn(function()
+    local place_Holder = nil
 
-while task.wait(1) do
+    for i,v in pairs(game.Players.LocalPlayer.Backpack:GetDescendants()) do
+       if v.Name == "InflictTarget" then
+            place_Holder = v
+       end
+    end;
+
+    while task.wait(1) do
         for i=1, #getgenv().LoopKill do
             pcall(function()
-        v = getgenv().LoopKill[i]
-place_Holder:FireServer(v.Character.Humanoid,v.Character.UpperTorso,math.huge,nil,0,false)
+                v = getgenv().LoopKill[i]
+                place_Holder:FireServer(v.Character.Humanoid,v.Character.UpperTorso,math.huge,nil,0,false)
+            end)
+        end
+    end
 end)
-end
-end
 local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wall%20v3')))()
 
 local w = library:CreateWindow("Some shit game")
@@ -61,6 +65,7 @@ end;
 
 pcall(function()
 place_Holder:FireServer(game.Players.LocalPlayer.Character.Humanoid,game.Players.LocalPlayer.Character.UpperTorso,-math.huge,nil,0,false)
+end)
 end)
 
 local Aimbot = loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/Aimbot-V3/main/src/Aimbot.lua"))()
