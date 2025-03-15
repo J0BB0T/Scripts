7ee2443eaffedea74928b0d6236ecc9b3fd09ec9
J0BB0T <155307865+J0BB0T@users.noreply.github.com>
2025-03-15 16:17:21 +0000

    Update NotorietyLoad.lua
    New Method

--- NotorietyLoad.lua
+++ NotorietyLoad.lua
@@ -2,8 +2,10 @@ game.Workspace.Criminals[game:GetService("Players").LocalPlayer.Name].Stamina.Va
 game:GetService("UserInputService").InputBegan:Connect(function(inp, proc)
     if proc then return end
     if inp.KeyCode == Enum.KeyCode.P then
-        for i, v in game:GetService("Workspace").Bags:GetChildren() do
-            v:MoveTo(game:GetService("Workspace").BagSecuredArea.FloorPart.Position)
+        for i,v in pairs(game:GetService("Workspace").Bags:GetDescendants()) do
+            if v.Name == 'MoneyBag' then
+                v.CFrame = game:GetService("Workspace").BagSecuredArea.FloorPart.CFrame * CFrame.new(0,0,10)
+            end
         end
     end
 end)
